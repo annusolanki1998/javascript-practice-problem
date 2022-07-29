0d1e162320c1ac5805b869427766324efb14818f
Annu <annusinghraj1998@gmail.com>
2022-07-30 01:03:53 +0530

    ADDED: Ability to performed unit conversion, found area of ractangle and square plot

---
+++ unitConversion.js
@@ -0,0 +1,18 @@
+const feet = 12;
+let answer = 42/12;
+console.log("The result is : " + answer);
+
+// b. Calculate area of rectangle 
+
+const length = 60 ;
+const breadth  = 40;
+const area = length * breadth;
+
+console.log("The area of rectangle plot in feet : " + area);
+
+// c. Calculate area of plots
+
+const side = 25;
+const area1 = side * side;
+
+console.log("The area of plot in acres is : " + area1);
